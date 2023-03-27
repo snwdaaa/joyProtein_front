51c2fe6cb50c91d7673190e7dabc4f899ebafa7c
SeungWoo Lee <20211922@edu.hanbat.ac.kr>
2023-03-27 07:42:39 +0000

    변경사항 - three.js 직접 추가 - test 폴더 삭제 - 프로틴 정보 페이지에 3d 뷰어 추가

--- joyprotein/src/main/resources/static/3dviewer.js
+++ joyprotein/src/main/resources/static/3dviewer.js
@@ -1,17 +1,21 @@
-import * as THREE from "https://unpkg.com/browse/three@0.150.1/build/three.module.js";
-import {GLTFLoader} from "https://unpkg.com/browse/three@0.150.1/examples/jsm/loaders/GLTFLoader.js";    // GLTF 로더 가져오기
+import * as THREE from 'three';
+import {GLTFLoader} from 'gltfLoader';
 
 // 렌더러
 const renderer = new THREE.WebGLRenderer({antialias: true});
-renderer.setSize(window.innerWidth, window.innerHeight);
-document.body.appendChild(renderer.domElement);
+const container = document.getElementById('viewer')
+let viewerOffset = -20;
+renderer.setSize(container.scrollWidth + viewerOffset, container.scrollHeight + viewerOffset);
+container.appendChild(renderer.domElement);
 
 // 씬
 const scene = new THREE.Scene();
+scene.background = new THREE.Color(0x5c6a79);
+scene.backgroundBlurriness = 0.5;
 
 // 카메라
 const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
-camera.position.set(0, 1, 2);
+camera.position.set(0, 1, 1);
 camera.lookAt(0, 0, 0)
 
 let light = new THREE.DirectionalLight(0xffffff, 20); //조명 
@@ -31,7 +35,7 @@ function animate()
 {
     requestAnimationFrame(animate);
 
-    object.rotation.y += 0.01;
+     object.rotation.y += 0.01;
 
     renderer.render(scene, camera);
 }
